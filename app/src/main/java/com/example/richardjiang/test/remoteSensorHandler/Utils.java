4e84ba4e32a7b9f36cbeed340edad5c55b77d071
RichardeJiang <richardejiang@hotmail.com>
2015-06-26 18:18:45 -0700

    test app partially finished, button awaits

---
+++ app/src/main/java/com/example/richardjiang/test/remoteSensorHandler/Utils.java
@@ -0,0 +1,16 @@
+package com.example.richardjiang.test.remoteSensorHandler;
+
+/**
+ * Created by Richard Jiang on 6/26/2015.
+ */
+public class Utils {
+    public static final String ACCURACY = "accuracy";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String VALUES = "values";
+    public static final String FILTER = "filter";
+
+    public static final String START_MEASUREMENT = "/start";
+    public static final String STOP_MEASUREMENT = "/stop";
+    public static final String SENSOR_DATA_PATH = "/sensor-data";
+    public static final String STORE_COMMAND = "get_type";
+}
